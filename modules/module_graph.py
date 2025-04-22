de3852038e48dc150f0c8cf0286497bc78bb1525
Sapehui0003 <sapehui200@gmail.com>
2025-04-22 13:58:40 -0600

    se creó la funcion de graph lr para graficar los coeficinetes del modelo

--- modules/module_graph.py
+++ modules/module_graph.py
@@ -2,13 +2,14 @@ import matplotlib.pyplot as plt
 from sklearn.tree import plot_tree
 import os
 import seaborn as sns
+import pandas as pd
 
 # MLFlow
 import mlflow
 from mlflow.models.signature import infer_signature
 
 # External libraries
-from module_path import plots_data_path 
+from module_path import plots_data_path , lr_plots_data_path
 
 #Sets the path to save the plots
 plots_path = plots_data_path()
@@ -36,6 +37,27 @@ def graph_confusion_matrix(cm, model, tag, labels=None):
     
     filename = f"conf_matrix_{model}_{tag}.png"
     full_path = os.path.join(plots_path, filename)
+    plt.savefig(full_path)
+    plt.close()
+
+    mlflow.log_artifact(full_path)
+
+def graph_lr_coefficients(coefficients, feature_names, model_name, tag):
+    """
+    Grafica los coeficientes de un modelo de Regresión Logística y guarda en la carpeta lr_plots.
+    """
+    feature_importance_df = pd.DataFrame({'Feature': feature_names, 'Coefficient': coefficients})
+    feature_importance_df = feature_importance_df.sort_values(by='Coefficient', ascending=False)
+
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x='Coefficient', y='Feature', data=feature_importance_df)
+    plt.title(f'Logistic Regression Coefficients - {model_name} {tag}')
+    plt.xlabel('Coefficient Magnitude')
+    plt.ylabel('Feature')
+    plt.tight_layout()
+
+    filename = f"lr_coefficients_{model_name}_{tag}.png"
+    full_path = os.path.join(lr_plots_data_path, filename) # Guarda en la carpeta lr_plots
     plt.savefig(full_path)
     plt.close()
 
