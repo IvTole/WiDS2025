1001ef1df5b8b7f787c377bb600f08e653796d54
FranciscoVazquez10 <franciscojaviervazquezflores@gmail.com>
2025-04-22 11:45:38 -0600

    Se cambiaron hiperparametros de ambos modelos por los de GridSearchCV

--- modules/run_classification.py
+++ modules/run_classification.py
@@ -49,13 +49,17 @@ def main():
     'max_iter': [1000, 2500, 5000]
     }
 
+    # Create grid for models
+    grid_lr = GridSearchCV(LogisticRegression(), param_grid_lr, cv=5, scoring='accuracy')
+    grid_rf = GridSearchCV(RandomForestClassifier(random_state=42), param_grid_rf, cv=5, scoring='accuracy')
+
     # evaluate models (adhd)
     ev = ModelEvaluation(X=df_train, y=labels[targets[0]], tag='adhd')
-    ev.evaluate_model(LogisticRegression(solver='lbfgs', max_iter=5000))
+    ev.evaluate_model(grid_lr)
 
     # evaluate models (sex_f)
     ev = ModelEvaluation(X=df_train, y=labels[targets[1]], tag='sex_f')
-    ev.evaluate_model(LogisticRegression(solver='lbfgs', max_iter=5000))
+    ev.evaluate_model(grid_lr)
     
     # prediction with test dataset
     sub = ModelSubmission(X=df_test, version=1, threshold=0.5, adhd_tag="adhd", sex_f_tag="sex_f")
@@ -63,19 +67,13 @@ def main():
 
     # Train and evaluate RandomForest for adhd
     rf_adhd = ModelEvaluation(X=df_train, y=labels[targets[0]], tag='rf_adhd')
-    rf_adhd.evaluate_model(RandomForestClassifier(
-        n_estimators=1000,
-        criterion="gini",
-        max_depth=10,
-        random_state=42,
-        bootstrap=True,
-    ))
+    rf_adhd.evaluate_model(grid_rf)
 
     # Train and evaluate RandomForest for sex_f
     rf_sex_f = ModelEvaluation(X=df_train, y=labels[targets[1]], tag='rf_sex_f')
     
     #sets the model 
-    model_to_evaluate= RandomForestClassifier(n_estimators=100, criterion="gini", max_depth=10, random_state=42, bootstrap=True)
+    model_to_evaluate= RandomForestClassifier(grid_lr)
     rf_sex_f.evaluate_model(model=model_to_evaluate)
     
     # Plots a tree of the forest
